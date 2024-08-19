52c833e44f4c4ef8ed6cda6200e9f0f6bfc09810
Jose Adrian <kerpador@gmail.com>
2024-08-19 02:34:02 -0600

    data

--- src/first/test.r
+++ src/first/test.r
@@ -1,1 +1,13 @@
-print('hello')
+#install.packages("ggplot2", repos = "https://cran.r-project.org")
+library(ggplot2)
+
+data<-read.csv('./database/Reg1.csv')
+head(data)
+print(summary(data))
+# Crear un gráfico de dispersión básico
+plot(data$x, data$y, 
+     xlab = "X", 
+     ylab = "y", 
+     main = "Scatterplot of Weight vs MPG",
+     pch = 19, # Cambia la forma de los puntos
+     ) # C
